97496cee102dd5e66b5f673440ac0c70bbdeef67
Filip Hnízdo <filip@bluejumpers.com>
2016-06-25 23:53:22 +0100

    Remove scroll speed cap

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,7 +1,5 @@
-var scrollSlow = 6;
-
 $("body,article,html").on("mousewheel", function (event) {
 
-  document.body.scrollLeft -= ((event.deltaY * event.deltaFactor) / scrollSlow);
+  document.body.scrollLeft -= ((event.deltaY * event.deltaFactor));
   event.preventDefault();
 });

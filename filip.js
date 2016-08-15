dcc0394a13b5951b62783ed48a902591c773cbd4
Filip Hnízdo <filip@bluejumpers.com>
2016-08-15 21:27:19 +0100

    New folder structure

---
+++ filip.js
@@ -0,0 +1,9 @@
+require("irisjs")({
+  "sitePath": "/filip",
+  "port": 5555,
+  "dbEngine": "nedb",
+  "siteEmail": "filip@filipnest.com",
+  "max_file_size": 50,
+  "cacheQueries": true,
+  "cacheTemplatePaths": 1000
+});
--- index.js
+++
@@ -1,7 +0,0 @@
-require("irisjs")({
-  "sitePath": "/iris",
-  "port": 5555,
-  "dbEngine":"nedb",
-  "siteEmail": "filip@filipnest.com",
-  "max_file_size": 50
-})

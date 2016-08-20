a3cfa2a7a119aba21f3dc9cb5c6c83872d447d50
Filip Hnízdo <filip@bluejumpers.com>
2016-08-20 17:30:57 +0100

    Link hypenation rules

--- filip/themes/filip/static/js/general.js
+++ filip/themes/filip/static/js/general.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
   $('p').hyphenate('en-gb');
   $('p span').hyphenate('en-gb');
+  $('p a').hyphenate('en-gb');
   $('p strong').hyphenate('en-gb');
   $('p strong a').hyphenate('en-gb');
 
